5c845144b36a9e35d848de010f639e902c239432
Nahue <nahuelrmb4@gmail.com>
2023-09-22 23:40:17 +0200

    first example the model for my appweb

---
+++ src/app/models/user.ts
@@ -0,0 +1,14 @@
+
+export class User {
+  id: number;
+  name: string;
+  lastName: string;
+  identNumer: number;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.lastName = '';
+    this.identNumer = 0;
+  }
+}
